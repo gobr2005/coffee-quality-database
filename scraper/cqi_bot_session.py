058d64ab071d04ee061cdd12b6c100154a00d08e
gobr2005 <83517458+gobr2005@users.noreply.github.com>
2022-02-08 19:53:31 -0500

    Update cqi_bot_session.py
    Updated code for CQI v.3 database. Updated some sleep times to allow "successful login" badge to leave.
    Updated syntax to meet new Selenium syntax rules.

--- scraper/cqi_bot_session.py
+++ scraper/cqi_bot_session.py
@@ -8,7 +8,7 @@ login_email = 'your_email_here'
 login_password = 'your_password_here'
 
 # open chromedriver
-driver = webdriver.Chrome('your_chromedriver_location_here')
+driver = webdriver.Chrome('chromedriver')
 time.sleep(2)
 
 # navigate to login page
@@ -16,21 +16,21 @@ driver.get('https://database.coffeeinstitute.org/login')
 time.sleep(2)
 
 # submit login credentials 
-form = driver.find_element_by_xpath('//html/body/content[@class="scrollable"]/div[@class="container page"]/div[@class="form short"]/div[@class="login panel"]/form')
-username = driver.find_element_by_name("username")
-password = driver.find_element_by_name("password")
+form = driver.find_element(By.XPATH, '//html/body/content[@class="scrollable"]/div[@class="container page"]/div[@class="form short"]/div[@class="login panel"]/form')
+username = driver.find_element(By.NAME, "username")
+password = driver.find_element(By.NAME, "password")
 time.sleep(2)
 
 username.send_keys(login_email)
 password.send_keys(login_password)
-driver.find_element_by_class_name("submit").click()
-time.sleep(2)
+driver.find_element(By.CLASS_NAME, "submit").click()
+time.sleep(6)
 
 
 # navigate to coffees page, then to arabicas page containing links to all quality reports 
-coffees = driver.find_element_by_xpath('//html/body/header/nav[@id="main"]/div[@class="container"]/div[@class="in"]/a[@href="/coffees"]').click()
-time.sleep(3)
-driver.find_element_by_link_text('Arabica Coffees').click()
+coffees = driver.find_element(By.XPATH, '//html/body/header/nav[@id="main"]/div[@class="container"]/div[@class="in"]/a[@href="/coffees"]').click()
+time.sleep(6)
+driver.find_element(By.LINK_TEXT ,'Arabica Coffees').click()
 time.sleep(3)
 
 # these values can be changed if this breaks midway through collecting data to pick up close to where you left off
@@ -40,25 +40,26 @@ coffeenum = 0
 while True:
 	print('page {}'.format(page))
 
-	# 50 rows in these tables * 7 columns per row = 350 cells. Every 7th cell clicks through to that coffee's data page
-	for i in range(0,350,7):
+	# 39 rows in these tables * 8 columns per row = 312 cells. Every 8th cell clicks through to that coffee's data page starting with the 2nd cell.
+	for i in range(1,312,8):
 		time.sleep(4)
 
 		# paginate back to the desired page number
 		# don't think there's a way around this - the back() option goes too far back
 		# some page numbers aren't available in the ui, but 'next' always is unless you've reached the end 
 		for p_num in range(page):
-			page_buttons = driver.find_elements_by_class_name('paginate_button')
+			page_buttons = driver.find_elements(By.CLASS_NAME, 'paginate_button')
 			page_buttons[-1].click() # the 'next' button
 			time.sleep(1)
-			page_buttons = driver.find_elements_by_class_name('paginate_button')
+			page_buttons = driver.find_elements(By.CLASS_NAME, 'paginate_button')
 
 		# select the cell to click through to the next coffee-data page
-		time.sleep(2) # this next line errors out sometimes, maybe it needs more of a time buffer 
-		test_page=driver.find_elements_by_xpath('//td')[i].click()
+		time.sleep(2) # this next line errors out sometimes, maybe it needs more of a time buffer
+		sample_num=driver.find_elements(By.XPATH, '//td')[i].text
+		test_page=driver.find_elements(By.XPATH, '//td')[i].click()
 		time.sleep(2)
 		print('rows: ')
-		print(len(driver.find_elements_by_xpath("//tr")))
+		print(len(driver.find_elements(By.XPATH, "//tr")))
 		tables = driver.find_elements(By.TAG_NAME, "table")
 
 		# loop over all coffee reports on the page, processing each one and writing to csv
@@ -88,7 +89,7 @@ while True:
 		coffeenum += 1
 
 	page += 1
-	if page == 27:
+	if page == 4:
 		break
 
 
